418ad3bb64cb2fba65b9fa906f766188c376545e
mirimir <mirimir@riseup.net>
2019-11-13 17:51:40 -0700

    Create prepare.sh

---
+++ utility_scripts/prepare.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt-get update
+apt-get -y install openvpn isc-dhcp-server iptables-persistent w3m
+sleep 5
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+sysctl -p
+cp /home/user/vpnchains/dhcp/dhcpd.conf /etc/dhcp/
+echo 'INTERFACES="enp0s8"' > /etc/default/isc-dhcp-server
