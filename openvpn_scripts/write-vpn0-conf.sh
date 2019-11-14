52972054efb1a1dddaaf1697e5d4045e296f5399
mirimir <mirimir@riseup.net>
2019-11-13 23:02:16 -0700

    Update write-vpn0-conf.sh

--- openvpn_scripts/write-vpn0-conf.sh
+++ openvpn_scripts/write-vpn0-conf.sh
@@ -4,7 +4,7 @@ IPV4S=`cat /etc/openvpn/scripts/vpns0`
 RPORT="1194"
 for IPV4 in $IPV4S
 do
-   echo "remote "$IPV4 >> /tmp/vpn0
+   echo "remote "$IPV4 > /tmp/vpn0
    echo "rport $RPORT" >> /tmp/vpn0
    cat /etc/openvpn/vpn0/base.conf >> /tmp/vpn0
    echo "auth-user-pass /etc/openvpn/vpn0/up" >> /tmp/vpn0
