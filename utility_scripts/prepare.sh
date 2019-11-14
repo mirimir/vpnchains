4b574a96d40fbffef008e6b2dd2016ac8d2ec28b
mirimir <mirimir@riseup.net>
2019-11-13 18:04:54 -0700

    Update prepare.sh

--- utility_scripts/prepare.sh
+++ utility_scripts/prepare.sh
@@ -7,3 +7,4 @@ echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sysctl -p
 cp /home/user/vpnchains/dhcp/dhcpd.conf /etc/dhcp/
 echo 'INTERFACES="enp0s8"' > /etc/default/isc-dhcp-server
+systemctl start isc-dhcp-server.service
