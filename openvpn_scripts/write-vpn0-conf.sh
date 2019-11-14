88adc02c7092ef7c1924724dc7833d6ecc95798f
mirimir <mirimir@riseup.net>
2019-11-13 23:42:30 -0700

    Update write-vpn0-conf.sh

--- openvpn_scripts/write-vpn0-conf.sh
+++ openvpn_scripts/write-vpn0-conf.sh
@@ -7,9 +7,12 @@ do
    echo "remote "$IPV4 > /tmp/vpn0
    echo "rport $RPORT" >> /tmp/vpn0
    cat /etc/openvpn/vpn0/base.conf >> /tmp/vpn0
+   # drop if VPN service doesn't use username/password authentication
    echo "auth-user-pass /etc/openvpn/vpn0/up" >> /tmp/vpn0
    echo "ca /etc/openvpn/vpn0/ca.crt" >> /tmp/vpn0
+   # drop if VPN service doesn't use client.crt
    echo "cert /etc/openvpn/vpn0/client.crt" >> /tmp/vpn0
+   # drop if VPN service doesn't use client.key
    echo "key /etc/openvpn/vpn0/client.key" >> /tmp/vpn0
    echo "tls-auth /etc/openvpn/vpn0/ta.key" >> /tmp/vpn0
    cat /tmp/vpn0 > /etc/openvpn/vpn0/$IPV4.conf
