238d9f42ecbd6ef24c18d024d8e16437d5a6ae6c
mirimir <mirimir@riseup.net>
2019-11-13 23:40:20 -0700

    Update write-vpn1-conf.sh

--- openvpn_scripts/write-vpn1-conf.sh
+++ openvpn_scripts/write-vpn1-conf.sh
@@ -9,7 +9,9 @@ do
    cat /etc/openvpn/vpn1/base.conf >> /tmp/vpn1
    echo "auth-user-pass /etc/openvpn/vpn1/up" >> /tmp/vpn1
    echo "ca /etc/openvpn/vpn1/ca.crt" >> /tmp/vpn1
+   # drop if VPN service doesn't use client.crt
    echo "cert /etc/openvpn/vpn1/client.crt" >> /tmp/vpn1
+   # drop if VPN service doesn't use client.key
    echo "key /etc/openvpn/vpn1/client.key" >> /tmp/vpn1
    echo "tls-auth /etc/openvpn/vpn1/ta.key" >> /tmp/vpn1
    cat /tmp/vpn1 > /etc/openvpn/vpn1/$IPV4.conf
