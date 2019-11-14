3b3c88c16bbeb0f9bca3f9ded59421bd4a0d22c7
mirimir <mirimir@riseup.net>
2019-11-13 22:10:15 -0700

    Update vpn1-base.sh

--- openvpn_scripts/vpn1-base.sh
+++ openvpn_scripts/vpn1-base.sh
@@ -3,4 +3,4 @@
 ip route del 0.0.0.0/1 && ip route del 128.0.0.0/1
 TUN0=`ip address show dev tun0 | grep -e "peer" | awk '{ print $4 }' | awk -F '/' '{ print $1 }'`
 ip route add VPN1 via $TUN0 dev tun0
-openvpn /etc/openvpn/vpn1/VPN1.conf &
+/sbin/openvpn /etc/openvpn/vpn1/VPN1.conf &
