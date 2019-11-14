b97c5683328334c4e049ba3fa67114212bd6d54d
mirimir <mirimir@riseup.net>
2019-11-13 22:43:39 -0700

    Update vpn0-base.sh

--- openvpn_scripts/vpn0-base.sh
+++ openvpn_scripts/vpn0-base.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ip route del 0.0.0.0/1 && ip route del 128.0.0.0/1
-ENP0S3=`ip r | grep -e "default" | sed 's/default//g'`
-ip route add VPN0 $ENP0S3
+/sbin/ip route del 0.0.0.0/1 && ip route del 128.0.0.0/1
+ENP0S3=`/sbin/ip r | grep -e "default" | sed 's/default//g'`
+/sbin/ip route add VPN0 $ENP0S3
 /sbin/openvpn /etc/openvpn/vpn0/VPN0.conf &
